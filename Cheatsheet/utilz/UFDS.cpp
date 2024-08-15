0755c00656c804368164c4f8b42f014a80ce590f
Kenny Arlexy <kennyarlexy@gmail.com>
2024-08-15 14:13:06 +0700

    implemented UFDS OOP style
    noticed and fixed problem with previous find() function,
    then reimplemented in OOP style

--- Cheatsheet/utilz/UFDS.cpp
+++ Cheatsheet/utilz/UFDS.cpp
@@ -2,27 +2,30 @@
 #define ll long long
 using namespace std;
 
-//for union find
-ll link[300005];
-ll sz[300005];
+struct UFDS {
+    vector<int> p, sz;
 
-// Method -> find(), same(), unite()
-// returns the representative of u
-ll find(ll u) {
-    if (link[u] == u) return u;
-    return link[u] = find(u);
-}
+    // n elements from [0..n-1]
+    UFDS(int n) : p(n+5), sz(n+5, 1) {
+        iota(p.begin(), p.end(), 0);
+    }
 
-// returns true if u and v belong to the same set (have the same representative)
-bool same(ll u, ll v) {
-    return find(u) == find(v);
-}
+    int find(int u) {
+        if (p[u] == u) return u;
+        return p[u] = find(p[u]);
+    }
 
-// combine set u and v where sz[u] > sz[v] with u as the new representative 
-void unite(ll u, ll v) {
-    u = find(u);
-    v = find(v);
-    if (sz[u] < sz[v]) swap(u, v);
-    link[v] = u;
-    sz[u] += sz[v];
-}
+    // returns true if u and v belong to the same set (have the same representative)
+    bool same(int u, int v) {
+        return find(u) == find(v);
+    }
+
+    // combine set u and v where sz[u] > sz[v] with u as the new representative 
+    void unite(int u, int v) {
+        u = find(u);
+        v = find(v);
+        if (sz[u] < sz[v]) swap(u, v);
+        p[v] = u;
+        sz[u] += sz[v];
+    }
+};
